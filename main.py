5da23d1d3657c4f813ce65e8c6c35a40297a44e1
Brandon Wieliczko <wieliczkMAC@Brandons-MacBook-Air.local>
2016-11-17 00:00:05 -0700

    Set up basic game, currently working on: drawBoard and checkWin

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ MAXHEIGHT = 8
 MAXROW = 8
 ACCEPTED = [0,1,2,3,4,5,6,7]
 QUITS = "exit"
+SUCCESS = 1
+FAIL = 0
+PLAYERONE = "x"
+PLAYERTWO = "o"
 
 class Board:
 	""" Board for the game"""
@@ -21,15 +25,24 @@ class Board:
 	
 	# Adds players move list
 	def chosenMove(self, choice, player):
-		if player == 0:
-			self.bRows[choice].append("x")
+		if not len(self.bRows[choice]) >= MAXHEIGHT:
+			if player == 0:
+				self.bRows[choice].append(PLAYERONE)
+			else:
+				self.bRows[choice].append(PLAYERTWO)
+			return SUCCESS
 		else:
-			self.bRows[choice].append("o")
+			return FAIL
 	
 	# For testing
 	def tprintb(self):
 		print(self.bRows)
-
+	# Draws Board 
+	def drawBoard(self):
+		print("")
+		# TODO 
+		
+# Where the player chooses which row to drop a piece in
 def makeMove(player):
 	while True:
 		if player == 1:
@@ -48,6 +61,12 @@ def makeMove(player):
 			print("Incorrect input")	
 	return Pchoice
 
+# Checks if player 
+def checkWin(board, userChoice):
+	print("")
+	# TODO COMPLETE 
+	
+
 def main():
 	board = Board()
 	board.createBoard()	
@@ -59,8 +78,12 @@ def main():
 			userChoice = int(userChoice)
 		except:
 			break # Only way to get here is through typing exit
-		board.chosenMove(userChoice, count%2)
-		count +=1
+		checkMax = board.chosenMove(userChoice, count%2)
+		if checkMax == SUCCESS:
+			
+			count +=1
+		else:
+			print("The maximum number of tiles in that row has been reach try again")
 	print("A player has chosen to quit")
 
 main()
