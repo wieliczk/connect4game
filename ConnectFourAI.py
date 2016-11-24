01b2ed2005e7f38d63cd3f0a07c9fa5b2a15110c
Androcks <andrewwsom@gmail.com>
2016-11-23 22:19:27 -0700

    Actually goes through depth and proper moves, needs to know when it can't make a move.

--- ConnectFourAI.py
+++ ConnectFourAI.py
@@ -1,5 +1,6 @@
-import main
-
+from main import Board
+from main import checkWin
+from copy import deepcopy
 
 #every nodename is a key to node object.
 
@@ -11,13 +12,18 @@ class Node:
         self.children = []
         self.parents = []
         self.negamax = None
+        self.depth = 0
 
-    def defineNode(self, boardconfig, parentsP, negaMax, childrenP=None):
-        self.name = self
+    def defineNode(self, name, boardconfig, parentsP, negaMax, depth=0, childrenP=[]):
+        self.name = name
         self.board = boardconfig
         self.children = [childrenP]
         self.parents = [parentsP]
         self.negamax = negaMax
+        self.depth = depth
+
+    def getNodeName(self):
+        return self.name
 
     def gotoChild(self,childIndex):
         return self.children[int(childIndex)]
@@ -25,9 +31,11 @@ class Node:
     def addChild(self, childNode):
         self.children.append(childNode)
 
-    def newChildNode(self, name, board):
-        name = name.Node()
-        name.defineNode(board, self, -999)
+    def newChildNode(self, childName, board):
+        node = Node()
+        parent = self.getNodeName()
+        node.defineNode(childName, board, parent, -999, self.getDepth() + 1)
+        return node
 
     def gotoParent(self, parentIndex):
         return self.parents[int(parentIndex)]
@@ -44,65 +52,71 @@ class Node:
     def getBoard(self):
         return self.board
 
+    def getDepth(self):
+        return self.depth
 
 
 
 #GameTree Builder Code
-
-gameTree = []
-
 #Sorts all nodenames in the same depth into a list, from 0 to 41.
-
-depthList = []
-
 #alpha/beta values will be assigned at beginning of game.
-
-alpha = None
-beta =  None
-_0V0 = Node()
-_0V0.defineNode([[],[],[],[],[],[],[],[]], -999, [])
-GameTree = [_0V0]
-nameCounter = 0
-depthList= [[]]
-
-
 #returns a boolean value of weather a board already has a symmetrical
-copy
-def breadthSymCheck(self, board, depth):
-    symBoard = board.reverse()
+#copy
+def breadthSymCheck(inBoard, depth):
+    symBoard = deepcopy(inBoard)
+    symBoard.bRows.reverse()
     for node in depthList[depth]:
-        if node.board == symBoard:
+        if node.board.bRows == symBoard.bRows:
             return True
         else:
             return False
 
 #returns a boolean value of weather a node with the board already exists in the depth
-def breadthChildCheck(self, board, depth):
+def breadthChildCheck(board, depth):
     for node in depthList[depth]:
         if node.board == board:
             return node
             break
         else:
             return None
-
-    
         
 
-#Depth-first node generation
 
-def depthCheck(node):
-    for number in node.name:
-        if number == "V":
-            depth = int(node.name[number + 1])
-            try:
-                depth = (depth*10) + int(node.name[number + 2])
-                return depth
-            except:
-                return depth
-    
-    
+#def depthCheck(node):
+#    name = node.getNodeName()
+#    print(name)
+#    count = 0
+ #   for number in name:
+  #      if number == "V":
+   #         depth = int(name[count + 1])
+    #        try:
+     #           depth = (depth*10) + int(name[count + 2])
+      #          return depth
+       #     except:
+        #        return depth
+        #count = count + 1
 
-def TreeGen(node, index=0):
+def labelCheck(node):
+    name = node.getNodeName()
+    return int(name[1:])
+        
+            
+        
+    
+alpha = None
+beta =  None
+_0 = Node()
+board = Board()
+board.createBoard()
+#print(board)
+_0.defineNode("_0", board, -999, [])
+#print(_0.getBoard())
+GameTree = [_0]
+depthList= [[]]
+
+print("-----------treeGen---------")
+
+
     #Create board of next possible node via chosenMove()
     #If there are nodes already, compare the board to the boards of each node.
     #If board is not symmetric or equal to boards already in depth level,
@@ -114,35 +128,60 @@ def TreeGen(node, index=0):
     #for new children (unexplored moves)
     #when the generation finally returns to top after all is done, print out
     #GameTree List
-    depth = depthCheck(node)
-    posBoard = board.chosenMove(index,(depth % 2))
-    depth = depthCheck(node)
+def treeGen(node, index):
+    print("NODE start")
+    #print(node)
+    print(node.getNodeName())
+    depth = node.getDepth()
+    print(depth)
+    board = node.getBoard()
+    newBoard = deepcopy(board)
+    newBoard.chosenMove(index, depth%2)
+    print(board.bRows)
+    print(newBoard.bRows)
+    
 
     #checks if it is a leaf node and if so , assigns a negamax.
-    if node.name != "_0V0":
-        winLoss = checkWin(posBoard,index)
+    if node.name != "_0":
+        winLoss = checkWin(board, index)
         if winLoss == 1:
             node.negaMaxAss(100)
-        if winLoss == 0:
+        elif winLoss == 0:
             node.negaMaxAss(-100)
-        if depthCheck(node) == 42:
+        elif depth == 42:
             node.negaMaxAss(0)
 
 
 
-    if breadthSymCheck(posBoard, depth) == True and index < 6:
-        TreeGen(node, index + 1)
+    #if breadthSymCheck(board, depth) and index < 6:
+      #  board.bRows
+       # treeGen(node, index + 1
+       #print("Sym Check")
         
-    sharedChild = breadthChildCheck(posBoard, depth)
+    #sharedChild = breadthChildCheck(board, depth)
 
-    if sharedChild != None:
-        sharedChild.addParent(node)
-        TreeGen(node, index + 1)
+    #if sharedChild != None:
+        #sharedChild.addParent(node)
+        #treeGen(node, index + 1)
+        #print("shared check")
 
     #the board is unique and thus the node is created.
-    TreeGen(node.newChildNode("_" + str(nameCounter) + 'V' + str(depth + 1)), posBoard)
-        
-    
+   
+    label = labelCheck(node)
+    label = str(label + 1)
+    child = node.newChildNode(str("_" + label), newBoard)
+    print("Node end")
+
+    if len(depthList) - 1 < child.getDepth():
+        depthList.append([])
+    depthList[child.getDepth()].append(child)
+    #print(newBoard.bRows)
+    treeGen(child, index)
+
+ 
+treeGen(_0, 0)
+
+
     
     
     
